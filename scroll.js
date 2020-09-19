079382b4e6d504809704411c8a8ffffd3bfe9355
Mike Wharton <mikemwharton@gmail.com>
2020-09-19 17:07:40 +0100

    working on fixed navbar not working yet

--- scroll.js
+++ scroll.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {navFixed()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.ClassList.add("sticky");
+  } else {
+    navbar.ClassList.remove("sticky");
+  }
+}
+
 document.getElementById("scroll").addEventListener("click", scrollDown);
 
 function scrollDown() {
